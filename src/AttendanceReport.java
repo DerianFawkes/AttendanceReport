605eb3c2df8fde3a7d3e09ee326ce05ae6e2b0e2
DerianFawkes <derianfawkes@yandex.ru>
2014-06-04 14:28:49 +0400

    Initial commit

---
+++ src/AttendanceReport.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Dima on 04.06.14.
+ */
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.hssf.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class AttendanceReport {
+    public static void main(String[] args) {
+    Workbook wb = new XSSFWorkbook();
+        
+    }
+}
